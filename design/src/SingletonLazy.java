3da33e2268af70dafdcaca5106281c322ce648ae
peakyliu <1173385814@qq.com>
2023-05-08 11:48:01 +0800

    change

--- design/src/SingletonLazy.java
+++ design/src/SingletonLazy.java
@@ -1,15 +1,12 @@
-//懒汉式，线程安全
+//懒汉式，线程安全,因为加锁之后可以防止多次实例化，但是也丢失了之前节约资源带来的好处
 public class SingletonLazy {
     private static SingletonLazy uniqueInstance = new SingletonLazy();
 
     private SingletonLazy() {
     }
 
-    public static synchronized SingletonLazy getSingletonLazy(){
-        if (uniqueInstance == null) {
-            uniqueInstance = new SingletonLazy();
+    public static synchronized SingletonLazy getUniqueInstance(){
 
-        }
         return uniqueInstance;
     }
 }
