a698b179de8194d91f9ac25d2a9bf4a933126014
peakyliu <1173385814@qq.com>
2023-05-08 15:18:05 +0800

    change

---
+++ design/src/single/SingletonSafe.java
@@ -0,0 +1,25 @@
+package single;
+
+//单例模式，懒汉式
+public class SingletonSafe {
+    private static SingletonSafe uniqueInstane;
+
+    private SingletonSafe() {
+    }
+
+    //这是线程不安全的情况
+//    public static SingletonSafe getUniqueInstance(){
+//        if (uniqueInstane == null) {
+//            uniqueInstane = new SingletonSafe();
+//        }
+//        return uniqueInstane;
+//    }
+
+    //这是线程安全的情况
+    public static synchronized SingletonSafe getUniqueInstance(){
+        if (uniqueInstane == null) {
+            uniqueInstane = new SingletonSafe();
+        }
+        return uniqueInstane;
+    }
+}
