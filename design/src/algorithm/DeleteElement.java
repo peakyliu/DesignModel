121dd34013ce168c028126b215927304dfd5e964
peakyliu <1173385814@qq.com>
2023-05-10 00:48:52 +0800

    change

---
+++ design/src/algorithm/DeleteElement.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+//给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度
+//元素可以改变无需考虑新数组中超出长度后面的元素；
+class DeleteElement {
+    public int removeElement(int[] nums,int val){
+        //定义慢指针
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex] = nums[fastIndex];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
+    }
+}
