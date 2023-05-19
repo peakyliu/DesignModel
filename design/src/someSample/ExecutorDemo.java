b898112781a14533e66d0f04180b3cc6373ebb8e
peakyliu <1173385814@qq.com>
2023-05-19 16:14:01 +0800

    changes

---
+++ design/src/someSample/ExecutorDemo.java
@@ -0,0 +1,24 @@
+package someSample;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Future<String> submit = executorService.submit(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "abc";
+        });
+
+        String s = submit.get();
+        System.out.println(s);
+        executorService.shutdown();
+    }
+}
