8921ae550dd93cef7e8ce6c38b6a07208018f9f0
peakyliu <1173385814@qq.com>
2023-10-12 10:16:34 +0800

    changes

--- design/src/single/Singleton.java
+++ design/src/single/Singleton.java
@@ -1,6 +1,7 @@
 package single;
 
 //双重校验锁，因为只需要被实例化一次就可以使用了，所以先判断uniqueInstance是不是已经被实例化了，如果没有被实例化，就需要对实例化的过程进行加锁
+//第二次加锁是因为在if判断和new创建的过程中，为了防止重复创建对象
 //volatile保证了可见性，禁止指令重排，保证即使是在多线程的情况下也能安全运行
 public class Singleton {
     private volatile static Singleton uniqueInstance;
@@ -8,9 +9,9 @@ public class Singleton {
     private Singleton() {
     }
 
-    public static Singleton getUniqueInstance(){
+    public static Singleton getUniqueInstance() {
         if (uniqueInstance == null) {
-            synchronized (Singleton.class){
+            synchronized (Singleton.class) {
                 if (uniqueInstance == null) {
                     uniqueInstance = new Singleton();
                 }
--- design/src/single/SingletonSafe.java
+++ design/src/single/SingletonSafe.java
@@ -2,6 +2,7 @@ package single;
 
 //单例模式，懒汉式
 public class SingletonSafe {
+    //volatile保证了可见性，而且防止了，JVM指令重排
     private volatile static SingletonSafe uniqueInstane;
 
     private SingletonSafe() {
@@ -15,7 +16,7 @@ public class SingletonSafe {
 //        return uniqueInstane;
 //    }
 
-    //这是线程安全的情况
+    //这是线程安全的情况，但是这里在方法上面加锁代价比较大，我们只需要第一次没有创建时候创建用到即可，之后直接返回创建的对象
     public static synchronized SingletonSafe getUniqueInstance(){
         if (uniqueInstane == null) {
             uniqueInstane = new SingletonSafe();
