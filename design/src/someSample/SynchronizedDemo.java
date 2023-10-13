ae6594bdb2663b3ecc91d635541d3bcc9d52cf06
peakyliu <1173385814@qq.com>
2023-10-13 11:58:08 +0800

    changes

--- design/src/someSample/SynchronizedDemo.java
+++ design/src/someSample/SynchronizedDemo.java
@@ -3,6 +3,7 @@ package someSample;
 import java.security.PublicKey;
 
 public class SynchronizedDemo {
+    //由于这里synchronized是可重入锁，方法1已经获取了对象锁，所以再次获取方法2的时候不会产生死锁的问题
     public static void main(String[] args) {
         SynchronizedDemo synchronizedDemo = new SynchronizedDemo();
         synchronizedDemo.method1();
