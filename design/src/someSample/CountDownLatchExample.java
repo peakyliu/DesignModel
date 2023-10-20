6547502ee1f244ab764ef9f93de59469cf49f101
peakyliu <1173385814@qq.com>
2023-10-20 17:33:51 +0800

    changes

--- design/src/someSample/CountDownLatchExample.java
+++ design/src/someSample/CountDownLatchExample.java
@@ -9,6 +9,8 @@ public class CountDownLatchExample {
     private static final int THREAD_COUNT= 500;
 
     public static void main(String[] args) throws InterruptedException {
+        // 创建一个具有固定线程数量的线程池对象（如果这里线程池的线程数量给太少的话你会发现执行的很慢）
+        // 只是测试使用，实际场景请手动赋值线程池参数
         ExecutorService threadPool = Executors.newFixedThreadPool(300);
         final CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
         for (int i = 0; i < THREAD_COUNT; i++) {
@@ -19,6 +21,7 @@ public class CountDownLatchExample {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
+                    //表示一个请求完成，如果不加的话会造成死锁，因为countDownLatch没有释放
                     countDownLatch.countDown();
                 }
             });
