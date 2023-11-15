d901ebf8214e2d2dea9c9dcd3969b39609f4a01e
peakyliu <1173385814@qq.com>
2023-11-15 16:37:44 +0800

    changes

--- design/src/someSample/StreamDemo.java
+++ design/src/someSample/StreamDemo.java
@@ -1,5 +1,6 @@
 package someSample;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,8 +9,9 @@ import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("abc", "def", "ghi","abc", "jkl", "mno");
+        List<String> strings = Arrays.asList("abc", "def", "ghi","abc", "jkl", "mno","ahk");
         Stream<String> stringStream = strings.stream().filter(s -> "abc".equals(s));
+        strings.stream().filter(s -> s.startsWith("a")).sorted().forEach(System.out::println);
         long count = stringStream.count();
         System.out.println(count);
 
